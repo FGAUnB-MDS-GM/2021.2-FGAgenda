265e76db3d917416af6d921393ff5fd2cc5cfdb2
leonardo <leonardomilov@gmail.com>
2022-04-13 22:48:42 -0300

    refatorando views e forms de todolist

--- fgagenda/todolist/forms.py
+++ fgagenda/todolist/forms.py
@@ -8,14 +8,6 @@ from meta.models import Meta
 
 class ToDoForms(forms.ModelForm):
     
-#    def min_validation(value):
-#        if len(value) < 1:
-#            raise ValidationError("{} não é valido, o campo não deve ser vazio.". format(value))
-    
-#    def __init__(self, *args, **kwargs):
-#        super(ToDoList, self).__init__(*args, **kwargs)
-#        self.fields["tarefa"].validators.append(min_validation)    
- 
     class Meta:
         model = ToDoList
         fields = [
@@ -35,28 +27,31 @@ class ToDoForms(forms.ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
-        super(ToDoForms, self).__init__(*args, **kwargs)
+        self.pk = kwargs.pop('pk', None)
+        super().__init__(*args, **kwargs)
         self.fields['meta'] = forms.ChoiceField(
-            required=True, label="Meta", 
+            required=True, label="Meta",
             choices=self.get_meta_names
         )
+        
 
     def get_meta_names(self):
-        names = []
-        for meta in Meta.objects.all():
-            names.append((meta.id, meta.nome))
+        nomes = []
         
-        return names
+        for meta in Meta.objects.filter(pk=self.pk):
+            nomes.append((meta.id, meta.nome))
+        
+        return nomes
 
     def clean_meta(self):
-      data = self.cleaned_data['meta']
+        data = self.cleaned_data['meta']
 
-      if not Meta.objects.filter(pk=data).exists():
-        raise ValidationError("A meta não existe!")
-      else:
-        data = Meta.objects.get(pk=data)
+        if not Meta.objects.filter(pk=data).exists():
+            raise ValidationError("A meta não existe!")
+        else:
+            data = Meta.objects.get(pk=data)
 
-      return data
+        return data
     
     def clean(self):
         cleaned_data = super(ToDoForms, self).clean()
@@ -69,5 +64,49 @@ class ToDoForms(forms.ModelForm):
             tarefa=tarefa
         ).exists():
             raise ValidationError("A tarefa já existe!")
+
+        return cleaned_data
+
+class EditaToDoList(forms.ModelForm):
+    
+    class Meta:
+        model = ToDoList
+        fields = [
+            "tarefa"
+        ]
+        widgets = {
+            "tarefa": forms.TextInput(attrs={
+                'class': "cria-meta-input",
+                'placeholder': 'Tarefa'
+            })
+        }
+
+    def __init__(self, *args, **kwargs):
+        self.meta_pk = kwargs.pop('meta_pk', None)
+        request = kwargs.pop('request', None)
+        self.conteudo_nova_tarefa = request.POST['conteudo']
+        #self.request.POST['conteudo']
+        super().__init__(*args, **kwargs)
+
+    def clean_tarefa(self):
+        data = self.cleaned_data['tarefa']
+
+        if not ToDoList.objects.filter(pk=data).exists():
+            raise ValidationError("A tarefa não existe!")
+        else:
+            data = ToDoList.objects.get(pk=data)
+
+        return data
+    
+    def clean(self):
+        cleaned_data = super(EditaToDoList, self).clean()
+
+        meta = Meta.objects.get(pk=self.meta_pk)
+        tarefa = cleaned_data.get('tarefa')
+
+        if ToDoList.objects.filter(meta=meta,tarefa=tarefa).exists():
+            raise ValidationError("A tarefa já existe!")
+        else:
+            ToDoList.objects.filter(tarefa=tarefa).update(tarefa=self.conteudo_nova_tarefa)
 
         return cleaned_data
